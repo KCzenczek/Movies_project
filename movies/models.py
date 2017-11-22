554417a57fbebf7406f5efa69ea035f5bf48393d
Kinga Czenczek <kinga.czenczek@gmail.com>
2017-11-22 13:07:20 +0100

    initial migration and Person model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=24)
+    last_name = models.CharField(max_length=24)
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
